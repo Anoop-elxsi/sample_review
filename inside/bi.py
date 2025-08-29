08164364372c48ee8c68de228c914897eaf470a6
38026 <anoop.s2@tataelxsi.co.in>
2025-08-29 10:41:48 +0530

    Made changes in button

--- inside/bi.py
+++ inside/bi.py
@@ -1,1 +1,10 @@
-# No changes made to the code
+def get_value():
+    """
+    This function retrieves a value from the user.
+    """
+    while True:
+        value = input("Enter a value: ")
+        if value.isdigit():
+            return value
+        else:
+            print("Invalid input. Please enter a number.")
